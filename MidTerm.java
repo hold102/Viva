9ddc66e3c0ba25e149ccab94fda1aa916dcd5c1c
hold102 <113155551+hold102@users.noreply.github.com>
2024-11-05 11:24:29 +0800

    Add files via upload

---
+++ MidTerm.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MidTerm {
+
+    public static void main(String args[]){
+        Scanner keyboard = new Scanner(System.in);
+
+        int X , Y , move ;
+        System.out.println("Enter the coordinate X and Y: ");
+
+        X = keyboard.nextInt();
+        Y = keyboard.nextInt();
+
+        System.out.println("Enter the move: ");
+
+
+
+    }
+}
