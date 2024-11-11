beeeb3eb8fd78fed562b9bb246143c0d8b996da0
Brendan Lee Cheng Jun <156674360+brendan1107@users.noreply.github.com>
2024-11-11 11:30:33 +0800

    Add files via upload
    VivaQ3 solved using Methods with complete pseudocode

--- VivaQ3.java
+++ VivaQ3.java
@@ -1,94 +1,119 @@
+
+import java.util.ArrayList;
 import java.util.Scanner;
-public class VivaQ3{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter an integer greater than 1: ");
-        int num = scanner.nextInt();
-        
-        if (num < 1){
-            System.out.println("Invalid input. Please enter an integer greater than 1.");
-            return;
-        }
-        
-        boolean prime = true;
-       for (int i = 2; i * i <= num; i++) {
-           if (num % i == 0){
-               prime = false;
-               break;
-           }
-        }
-       if (prime){
-           System.out.println("Integer is a prime number. ");
-           System.out.println("Prime numbers between 2 and " + num + ": ");
-           for (int i = 2; i < num; i++) {
-                boolean primeCheck = true;
-                for (int j = 2; j <= Math.sqrt(i); j++) {
-                    if (i % j == 0) {
-                        primeCheck = false;
-                        break;
-                        }
-                }
-                if (primeCheck) {
-                    System.out.print(i + " ");
-                }
-            }
-            System.out.println();
-            return;
-       }
-       else {
-           System.out.println("Integer is not a prime number.");
-       }
-      
-        int factor = 0;
-        long sum = 0;
-        long product = 1;
-        boolean overflow = false;
-        final long overflowThreshold = 1_000_000_000;
 
-        System.out.print("The factors of this integer are: ");
-        for (int i = 1; i <= num; i++) {
-            if (num % i == 0) {
-                System.out.print(i + " ");
-                factor++;
+public class VivaQ3 {
+
+    // Method to find factors and return them in an ArrayList
+    public static ArrayList<Integer> factors(int input) {
+        ArrayList factorsArrayList = new ArrayList<>();
+        for (int i = 1; i <= input; i++) {
+            if (input % i == 0) {
+                factorsArrayList.add(i); // Adding factors into ArrayList
+            }
+        }
+        return factorsArrayList;
+    }
+
+    // Method to determine if input is prime
+    public static boolean isPrimeNumber(int input) {
+        return factors(input).size() == 2; //definition of prime number
+    }
+
+    // Method to find sum of factors
+    public static int factorsSum(int input) {
+        int sum = 0;
+        for (int i = 1; i <= input; i++) {
+            if (input % i == 0) {
                 sum += i;
-                
-                if (!overflow && product <= overflowThreshold / i) {
-                    product *= i;
+            }
+        }
+        return sum;
+    }
+
+    // Method to find product of factors
+    public static long factorsProduct(int input) {
+        long product = 1;
+        for (int i = 1; i <= input; i++) {
+            if (input % i == 0) {
+                product *= i;
+            }
+        }
+        return product;
+    }
+
+    // Method to determine if input is a perfect number
+    public static boolean isPerfectNum(int input) {
+        return input == factorsSum(input) - input; //definition of perfectNum
+    }
+
+    // Method to list prime numbers between 2 and input
+    public static ArrayList listPrimeNumber(int input) {
+        ArrayList<Integer> primeNumbers = new ArrayList<>();
+        for (int i = 2; i <= input; i++) {
+            if (factors(i).size() == 2) {
+                primeNumbers.add(i);
+            }
+        }
+        return primeNumbers;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input;
+
+        // Loop to check validity of input
+        while (true) {
+            System.out.println("Enter the number[Integer greater than positive one]:");
+            input = sc.nextInt();
+
+            // If the input is valid (input > 1), close the scanner to avoid memory leak
+            // and break the loop to proceed.
+            if (input > 1) {
+                sc.close();
+                break;
+            } else {
+                // If the input is invalid, prompt the user to re-enter
+                System.out.println("Invalid input, please try again.");
+            }
+        }
+
+        // Let numberOfFactors represent the number of factors obtained from the size of factors() ArrayList
+        int numberOfFactors = factors(input).size();
+
+        // Let sumOfFactors be the sum of factors from the factorsSum method
+        int sumOfFactors = factorsSum(input);
+
+        // Let productOfFactors be the product of sum from factorsProduct method
+        long productOfFactors = factorsProduct(input);
+
+        // Check if the input is a prime number
+        if (!isPrimeNumber(input)) {
+            // If it is not a prime number, print properties (isPrimeNumber?, list of factors, etc.)
+            System.out.println(input + " is not a prime number.");
+            System.out.println("Number of factors of " + input + ": " + numberOfFactors);
+            System.out.println("The factors of " + input + ": " + factors(input));
+            System.out.println("Sum of factors: " + sumOfFactors);
+
+            // In case of overflow, display a message indicating the product is too large to be displayed
+            if (productOfFactors == 0) {
+                System.out.println("The product of factors is too large to be displayed.");
+            } else {
+                System.out.println("Product of factors: " + productOfFactors);
+
+                // Check if the number is a perfect number
+                if (isPerfectNum(input)) {
+                    System.out.println(input + " is a perfect number");
                 } else {
-                    overflow = true;
+                    System.out.println(input + " is not a perfect number");
                 }
             }
-        }
-        System.out.println();
-        System.out.println("The integer has " + factor + " factors.");
-        System.out.println("The sum of the factors is " + sum);
-
-        if (overflow) {
-            System.out.println("The product of the factors is too large to display.");
         } else {
-            System.out.println("The product of the factors is " + product);
+            // If it is a prime number
+            System.out.println(input + " is a prime number.");
         }
 
-        long properDivisorSum = sum - num;
-        if (properDivisorSum == num) {
-            System.out.println(num + " is a perfect number.");
-        } else {
-            System.out.println(num + " is not a perfect number.");
-        }
-
-        System.out.print("Prime numbers between 2 and " + num + ": ");
-        for (int i = 2; i < num; i++) {
-            boolean primeCheck = true;
-            for (int j = 2; j <= Math.sqrt(i); j++) {
-                if (i % j == 0) {
-                    primeCheck = false;
-                    break;
-                }
-            }
-            if (primeCheck) {
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println();
+        // Print the list of prime numbers between 2 and the input
+        System.out.println("The prime numbers between 2 and " + input + ": " + listPrimeNumber(input));
     }
 }
