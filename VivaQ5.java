84fd9cb28294f4ecd16ce090d3b8e98bba29c7e0
yiuernnnn <yiuernnn@gmail.com>
2024-11-12 20:09:22 +0800

    Update VivaQ5.java

--- VivaQ5.java
+++ VivaQ5.java
@@ -1,29 +1,22 @@
 import java.util.Scanner;
 public class VivaQ5 {
-   public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Name entered should be less than 200 characters,all in uppercase and must not be empty.");
         System.out.print("Enter a name :");
-        String name =sc.nextLine();
+        String name = sc.nextLine();
         
-        // getting modified song name from user and check whether is it all in upper case(!sentence.equals(sentence.toUpperCase()).)
-        do{
-            System.out.print("Enter the name in correct format :");
-            name= sc.nextLine(); // getting user input
-        } while(name!=name.toUpperCase() || name.isEmpty() || name.length()>200 ||name.matches(".*\\s{1,}.*"));
-       
-        // split the words when REMIX is detected 
-        String modifiedname []=name.split("REMIX"); 
-        String oriname ="";
-        for(int i=0;i<modifiedname.length;i++){
-            oriname=oriname+" "+modifiedname[i];
+        //loop to check validity of the input
+        while (name.length() > 200 || !name.matches("[A-Z]+") || name.isEmpty()) {
+            System.out.print("Enter name in correct format :");
+            //prompt user to re-enter
+            name = sc.nextLine();   
         }
         
-        // removing any leading or ending white space
-        String finaloriname=oriname.trim();
-        
-        System.out.println(finaloriname);
-
-        
-        
-    }}
+        //replace all "REMIX" in the name to be space and remove all of the leading white space and ending space
+        String oriName = name.replaceAll("REMIX(\\s*REMIX)*", " ").trim(); /*REMIX(\\s*REMIX)* , this actually detects all subsequent  
+        "REMIX" at the same time including the whitespaces and replace them together with " " ( \\s means whitespace, \\s means multiple white spaces*/
+        //print the original name
+        System.out.println(oriName);
+    }
+}
